89734f4727aff2258fdfac379258dc55c1a23e6e
Eduard Klementiev <eklementiev@gmail.com>
2012-12-14 18:07:37 +0400

    Support multiline text blocks

--- app/models/ PdfEditor.scala
+++ app/models/ PdfEditor.scala
@@ -18,27 +18,31 @@ object PdfEditor {
         val output = new ByteArrayOutputStream()
         val stamper = new PdfStamper(reader, output)
         textBlocks.foreach {block =>
-            val cb = stamper.getOverContent(block.page)
-            cb.beginText()
-            val bf = BaseFont.createFont(Play.configuration.getString("fonts_path").get + "/" + block.font + ".ttf", BaseFont.IDENTITY_H, true)
-            val rec = reader.getCropBox(block.page)
-            val width = rec.getWidth()
-            val k = Play.configuration.getInt("page_width").get / width
+            var lineNumber = 0
+            block.text.lines.foreach {line => 
+                val cb = stamper.getOverContent(block.page)
+                cb.beginText()
+                val bf = BaseFont.createFont(Play.configuration.getString("fonts_path").get + "/" + block.font + ".ttf", BaseFont.IDENTITY_H, true)
+                val rec = reader.getCropBox(block.page)
+                val width = rec.getWidth()
+                val k = Play.configuration.getInt("page_width").get / width
 
-            val color = new BaseColor(
-                Integer.parseInt(block.color.substring(0, 2), 16), 
-                Integer.parseInt(block.color.substring(2, 4), 16), 
-                Integer.parseInt(block.color.substring(4, 6), 16), 
-                (if (block.color.size > 6) {Integer.parseInt(block.color.substring(6, 8), 16)} else {255})
-            )
+                val color = new BaseColor(
+                    Integer.parseInt(block.color.substring(0, 2), 16), 
+                    Integer.parseInt(block.color.substring(2, 4), 16), 
+                    Integer.parseInt(block.color.substring(4, 6), 16), 
+                    (if (block.color.size > 6) {Integer.parseInt(block.color.substring(6, 8), 16)} else {255})
+                )
 
-            cb.setFontAndSize(bf, round(block.fontSize / k))
-            cb.setColorFill(color)
-            val xx = round(block.x / k)
-            val yy = round(rec.getHeight() - block.y / k - block.fontSize / k)
-            cb.setTextMatrix(xx, yy)
-            cb.showText(block.text)
-            cb.endText()
+                cb.setFontAndSize(bf, round(block.fontSize / k))
+                cb.setColorFill(color)
+                val xx = round(block.x / k)
+                val yy = round(rec.getHeight() - block.y / k - block.fontSize / k)
+                cb.setTextMatrix(xx, yy - round(1.0*(block.fontSize+4)*lineNumber/k))
+                lineNumber += 1
+                cb.showText(line)
+                cb.endText()
+            }
         }
         stamper.close()
         output.toByteArray
