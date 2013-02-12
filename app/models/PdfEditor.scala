7043631e3d043bd922d77d795b7b86a6f19cb8ff
Eduard Klementiev <eklementiev@gmail.com>
2013-02-12 15:46:56 +0400

    Fix indentation(2 spaces)

--- app/models/PdfEditor.scala
+++ app/models/PdfEditor.scala
@@ -14,55 +14,55 @@ import java.net.URL
 
 object PdfEditor {
 
-    def addBlocks(pdfPath: String, blocks: List[Block]) = {
-        val reader = new PdfReader(pdfPath)
-        val output = new ByteArrayOutputStream()
-        val stamper = new PdfStamper(reader, output)
+  def addBlocks(pdfPath: String, blocks: List[Block]) = {
+    val reader = new PdfReader(pdfPath)
+    val output = new ByteArrayOutputStream()
+    val stamper = new PdfStamper(reader, output)
 
-        blocks.foreach {block =>
-            val cb = stamper.getOverContent(block.page)
-            val rec = reader.getCropBox(block.page)
-            val width = rec.getWidth()
-            val k = Play.configuration.getInt("page_width").get / width
+    blocks.foreach {block =>
+      val cb = stamper.getOverContent(block.page)
+      val rec = reader.getCropBox(block.page)
+      val width = rec.getWidth()
+      val k = Play.configuration.getInt("page_width").get / width
 
-            block match {
-                case textBlock: TextBlock => {
-                    var lineNumber = 0
-                    textBlock.text.lines.foreach {line => 
-                        cb.beginText()
-                        val bf = BaseFont.createFont(Play.configuration.getString("fonts_path").get + "/" + textBlock.font + ".ttf", BaseFont.IDENTITY_H, true)
+      block match {
+        case textBlock: TextBlock => {
+          var lineNumber = 0
+          textBlock.text.lines.foreach {line => 
+              cb.beginText()
+              val bf = BaseFont.createFont(Play.configuration.getString("fonts_path").get + "/" + textBlock.font + ".ttf", BaseFont.IDENTITY_H, true)
 
-                        val color = new BaseColor(
-                            Integer.parseInt(textBlock.color.substring(0, 2), 16), 
-                            Integer.parseInt(textBlock.color.substring(2, 4), 16), 
-                            Integer.parseInt(textBlock.color.substring(4, 6), 16), 
-                            (if (textBlock.color.size > 6) {Integer.parseInt(textBlock.color.substring(6, 8), 16)} else {255})
-                        )
+              val color = new BaseColor(
+                  Integer.parseInt(textBlock.color.substring(0, 2), 16), 
+                  Integer.parseInt(textBlock.color.substring(2, 4), 16), 
+                  Integer.parseInt(textBlock.color.substring(4, 6), 16), 
+                  (if (textBlock.color.size > 6) {Integer.parseInt(textBlock.color.substring(6, 8), 16)} else {255})
+              )
 
-                        cb.setFontAndSize(bf, round(textBlock.fontSize / k))
-                        cb.setColorFill(color)
-                        val xx = round(textBlock.x / k)
-                        val yy = round(rec.getHeight() - textBlock.y / k - textBlock.fontSize / k)
-                        cb.setTextMatrix(xx, yy - round(1.0*(textBlock.fontSize+4)*lineNumber/k))
-                        lineNumber += 1
-                        cb.showText(line)
-                        cb.endText()
-                    }
-                }
-
-                case imageBlock: ImageBlock => {
-                    val pdfImage = com.itextpdf.text.Image.getInstance(new URL(imageBlock.url))
-
-                    val xx = round(imageBlock.x / k)
-                    val yy = round(rec.getHeight() - imageBlock.y / k - imageBlock.height / k)
-
-                    cb.addImage(pdfImage, imageBlock.width / k, 0, 0, imageBlock.height/k, xx, yy)                    
-                }
-            }
+              cb.setFontAndSize(bf, round(textBlock.fontSize / k))
+              cb.setColorFill(color)
+              val xx = round(textBlock.x / k)
+              val yy = round(rec.getHeight() - textBlock.y / k - textBlock.fontSize / k)
+              cb.setTextMatrix(xx, yy - round(1.0*(textBlock.fontSize+4)*lineNumber/k))
+              lineNumber += 1
+              cb.showText(line)
+              cb.endText()
+          }
         }
 
-        stamper.close()
-        output.toByteArray
+        case imageBlock: ImageBlock => {
+          val pdfImage = com.itextpdf.text.Image.getInstance(new URL(imageBlock.url))
+
+          val xx = round(imageBlock.x / k)
+          val yy = round(rec.getHeight() - imageBlock.y / k - imageBlock.height / k)
+
+          cb.addImage(pdfImage, imageBlock.width / k, 0, 0, imageBlock.height/k, xx, yy)
+        }
+      }
     }
+
+    stamper.close()
+    output.toByteArray
+  }
 
 }
--- app/models/TextBlock.scala
+++ app/models/TextBlock.scala
@@ -1,11 +1,11 @@
 package models
 
 class TextBlock(
-    val text: String,
-    val font: String,
-    val fontSize: Int,
-    val color: String,
-    val page: Int,
-    val x: Int,
-    val y: Int
+  val text: String,
+  val font: String,
+  val fontSize: Int,
+  val color: String,
+  val page: Int,
+  val x: Int,
+  val y: Int
 ) extends Block
